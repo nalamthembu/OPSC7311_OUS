5c2b0a40b85588b7185f4442d2900dcdcde4d9b7
CellHanKS <88459746+CellHanKS@users.noreply.github.com>
2023-09-24 17:06:29 +0200

    another one

--- app/src/main/java/com/example/opsc7311_poe_part2/HomeScreenActivity.kt
+++ app/src/main/java/com/example/opsc7311_poe_part2/HomeScreenActivity.kt
@@ -2,10 +2,15 @@ package com.example.opsc7311_poe_part2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
-class HomeScreenActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class HomeScreenActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+
     }
 }
