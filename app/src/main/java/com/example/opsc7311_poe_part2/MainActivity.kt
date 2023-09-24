1d0e877f2aa98cbdecd828b9051a09725b71390f
Nala Mthembu <Nala00.12@gmail.com>
2023-09-24 20:43:54 +0200

    Login Screen Iteration

--- app/src/main/java/com/example/opsc7311_poe_part2/MainActivity.kt
+++ app/src/main/java/com/example/opsc7311_poe_part2/MainActivity.kt
@@ -4,16 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity()
 {
-    var btnLogin: Button?= null
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    var btnLogin: Button?= null;
+    var username: TextView? = null;
+    var password: TextView? = null;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnLogin = findViewById(R.id.btnLogIn)
+        //Text View Code (Username, Password)
+        username = findViewById(R.id.txtUsername);
+        password = findViewById(R.id.txtPassword);
+
+        //Login Button Code
+        btnLogin = findViewById(R.id.btn_login)
 
         btnLogin?.setOnClickListener()
         {
