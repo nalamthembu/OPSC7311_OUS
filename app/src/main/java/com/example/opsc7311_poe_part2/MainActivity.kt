d3f7506a21ddb7ca2c2a0252fb53ec3295725634
CellHanKS <88459746+CellHanKS@users.noreply.github.com>
2023-09-24 15:06:33 +0200

    Activity Switch

--- app/src/main/java/com/example/opsc7311_poe_part2/MainActivity.kt
+++ app/src/main/java/com/example/opsc7311_poe_part2/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.opsc7311_poe_part2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    var btnLogin: Button?= null
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //test ye
+        btnLogin = findViewById(R.id.btnLogIn)
+
+        btnLogin?.setOnClickListener()
+        {
+            startActivity(Intent(this, HomeScreenActivity::class.java))
+        }
     }
 }
