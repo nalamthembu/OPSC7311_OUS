f418961679e929a7e2d4e79d759fcbd10205a849
CellHanKS <88459746+CellHanKS@users.noreply.github.com>
2023-09-26 17:24:36 +0200

    Nav Menu

--- app/src/main/java/com/example/opsc7311_poe_part2/HomeScreenActivity.kt
+++ app/src/main/java/com/example/opsc7311_poe_part2/HomeScreenActivity.kt
@@ -1,16 +1,41 @@
 package com.example.opsc7311_poe_part2
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class HomeScreenActivity : AppCompatActivity()
 {
+    lateinit var burger_menu: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
 
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navigation : NavigationView = findViewById(R.id.navView)
 
+        burger_menu = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(burger_menu)
+        burger_menu.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(burger_menu.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
