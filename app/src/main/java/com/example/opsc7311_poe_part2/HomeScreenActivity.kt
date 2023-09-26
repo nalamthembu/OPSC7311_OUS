c6ad99f80131928a0732deb58bcb0427757974d8
CellHanKS <88459746+CellHanKS@users.noreply.github.com>
2023-09-26 23:21:05 +0200

    Pop UP

--- app/src/main/java/com/example/opsc7311_poe_part2/HomeScreenActivity.kt
+++ app/src/main/java/com/example/opsc7311_poe_part2/HomeScreenActivity.kt
@@ -3,7 +3,10 @@ package com.example.opsc7311_poe_part2
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
+import android.view.WindowManager
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.Toast
@@ -13,16 +16,25 @@ import com.google.android.material.navigation.NavigationView
 
 class HomeScreenActivity : AppCompatActivity()
 {
+    //Burger Menu Vars
     lateinit var burger_menu: ActionBarDrawerToggle
+
+    //Create Project Vars
+    var add_project: ImageButton ?= null
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
 
+        //Burger Menu Sets
         val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
         val navigation : NavigationView = findViewById(R.id.navView)
 
+        //Create Project Sets
+        add_project = findViewById(R.id.Add_Project)
+
+        //Burger Menu Action Bar
         burger_menu = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
         drawerLayout.addDrawerListener(burger_menu)
         burger_menu.syncState()
@@ -36,6 +48,24 @@ class HomeScreenActivity : AppCompatActivity()
             true
         }
 
+        //Create Project Pop Up
+        add_project?.setOnClickListener()
+        {
+            val popupInflater = LayoutInflater.from(applicationContext)
+            val viewPopup = popupInflater.inflate(R.layout.create_project,null)
+            val containerPopup = findViewById<RelativeLayout>(R.id.rel_layout)
+
+            val layoutParams = RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+            )
+
+            layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT)
+
+            viewPopup.layoutParams = layoutParams
+
+            containerPopup.addView(viewPopup)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
