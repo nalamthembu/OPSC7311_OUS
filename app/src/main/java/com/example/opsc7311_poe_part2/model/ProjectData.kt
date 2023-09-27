b7408f406d48d101308f4073185817ed9e43367b
slinkwolf <91013757+slinkwolf@users.noreply.github.com>
2023-09-27 20:36:08 +0200

    yeye

--- app/src/main/java/com/example/opsc7311_poe_part2/model/ProjectData.kt
+++ app/src/main/java/com/example/opsc7311_poe_part2/model/ProjectData.kt
@@ -1,8 +1,10 @@
 package com.example.opsc7311_poe_part2.model
 
+import android.icu.text.DateFormat
+import android.widget.EditText
 import java.util.Date
 
 data class ProjectData(
     val projectName:String,
-    val projectDueDate:Date
+    val projectDueDate:String
 )
--- app/src/main/java/com/example/opsc7311_poe_part2/view/UserAdapter.kt
+++ app/src/main/java/com/example/opsc7311_poe_part2/view/UserAdapter.kt
@@ -1,13 +1,42 @@
 package com.example.opsc7311_poe_part2.view
 
+import android.content.Context
+import android.service.autofill.UserData
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.opsc7311_poe_part2.R
-class UserAdapter()
+import com.example.opsc7311_poe_part2.model.ProjectData
+import java.text.DateFormat
+import java.util.Date
+
+class UserAdapter(val c:Context,val userList: ArrayList<ProjectData>): RecyclerView.Adapter<UserAdapter.userViewHolder>()
 {
     inner class userViewHolder(val v:View):RecyclerView.ViewHolder(v)
     {
-        val project_Name = v.findViewById<TextView>(R.id.project_Name)
+        val Name = v.findViewById<TextView>(R.id.project_Name)
+        val DueDate = v.findViewById<TextView>(R.id.project_dueDate)
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): userViewHolder
+    {
+        val inflater = LayoutInflater.from(parent.context)
+        val v = inflater.inflate(R.layout.project_item,parent,false)
+        return userViewHolder(v)
+    }
+    override fun onBindViewHolder(holder: userViewHolder, position: Int)
+    {
+        val newList = userList[position]
+        holder.Name.text = newList.projectName
+        holder.DueDate.text = newList.projectDueDate
+    }
+    override fun getItemCount(): Int
+    {
+        return userList.size
+    }
+
+
 }
