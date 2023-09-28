75f00f88a79ca5a0a810b0499b475c619f83a0c9
Nala Mthembu <Nala00.12@gmail.com>
2023-09-29 00:49:11 +0200

    Revert "Merge branch 'master' of https://github.com/nalamthembu/OPSC7311_OUS"
    This reverts commit 27c1f7ca63b0a7dc0326beec50bc4384a071abb3, reversing
    changes made to b5464c8a894e47f4a12f5bc238f470a84aa6f1fa.

--- app/src/main/java/com/example/opsc7311_poe_part2/DGoals.kt
+++ app/src/main/java/com/example/opsc7311_poe_part2/DGoals.kt
@@ -4,32 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
-import android.widget.NumberPicker
 
 class DGoals : AppCompatActivity() {
-    //Home Button
-    var buttonHome : ImageButton ?= null
 
-    //Number Pickers
-    lateinit var numPickHs : NumberPicker
-    lateinit var numPickMs : NumberPicker
-    val numPickFormatter : NumberPicker.Formatter = NumberPicker.Formatter { String.format("%02d", it) }
+    var buttonHome : ImageButton ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dgoals)
 
         buttonHome = findViewById(R.id.ib_dg_home)
-
-        numPickHs = findViewById(R.id.numPickerHours)
-        numPickHs.minValue = 0
-        numPickHs.maxValue = 59
-        numPickHs.setFormatter(numPickFormatter)
-
-        numPickMs = findViewById(R.id.numPickerMinutes)
-        numPickMs.minValue = 0
-        numPickMs.maxValue = 59
-        numPickMs.setFormatter(numPickFormatter)
 
         buttonHome?.setOnClickListener(){
             onBackPressed()
