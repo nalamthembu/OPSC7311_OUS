ab100c84b2f448ea05ec8d9bd5a5dcfc4cf0bd66
CellHanKS <88459746+CellHanKS@users.noreply.github.com>
2023-12-04 16:54:53 +0200

    Update TimelineView.kt

--- app/src/main/java/com/example/opsc7311_poe_part2/TimelineView.kt
+++ app/src/main/java/com/example/opsc7311_poe_part2/TimelineView.kt
@@ -20,6 +20,7 @@ import com.github.mikephil.charting.components.Description
 import com.google.firebase.Firebase
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 
@@ -31,60 +32,82 @@ class TimelineView : AppCompatActivity() {
     lateinit var linedata: LineDataSet
     lateinit var lineset:LineData
     lateinit var linechart:LineChart
+
+    lateinit var entries1List: ArrayList<Entry>
+    lateinit var linedata1set:LineDataSet
+
+    lateinit var entries2List: ArrayList<Entry>
+    lateinit var linedata2set: LineDataSet
+
     var convert : Float = 0f
+    var conv2:Float=0f
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timeline_view)
-        buttonHome = findViewById(R.id.ib_tv_home)
+
+        // Initialize other views and variables...
         linechart = findViewById(R.id.line_chart)
 
         val database = Firebase.database
         val readMin = database.getReference("DailyGoalMin")
         val readMax = database.getReference("DailyGoalMax")
 
+        val data = ArrayList<Entry>()
 
-        data = ArrayList<Entry>()
+        // Fetch data from the first source
+        fetchData(readMin, data)
 
-        readMin.addValueEventListener(object: ValueEventListener
-        {
-            override fun onDataChange(snapshot: DataSnapshot)
-            {
-                for(childSnapshot in snapshot.children)
-                {
+        // Fetch data from the second source
+        fetchData(readMax, data)
+    }
+
+    private fun fetchData(reference: DatabaseReference, data: ArrayList<Entry>) {
+        reference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // Clear existing data
+                data.clear()
+
+                for (childSnapshot in snapshot.children) {
                     val h = childSnapshot.child("hours").value
                     val m = childSnapshot.child("mins").value
                     val hoursval = h.toString()
                     val minsval = m.toString()
-                    convert = (hoursval.toFloat() * 100) + (minsval.toFloat())
+                    val convert = (hoursval.toFloat() * 100)
                     println(convert)
                     Log.d("my_data", "data $convert")
-                    data.add(Entry(0f,convert))
-                    data.add(Entry(100f,convert))
-                }
-                linedata = LineDataSet(data,"Entries")
-                linechart.description.text = "Hours worked"
-                lineset = LineData(linedata)
 
-                linechart.data = lineset
-                linedata.setColors(ColorTemplate.MATERIAL_COLORS,255)
-                linedata.valueTextColor = Color.WHITE
-                linechart.notifyDataSetChanged()
-                linechart.invalidate()
+                    // Add data to the list inside the callback
+                    data.add(Entry(25f, convert))
+                }
+
+                if(data.isNotEmpty())
+                    updateChart()
             }
 
             override fun onCancelled(error: DatabaseError) {
                 println("Firebase Error: " + error.message)
             }
-        }
-        )
+        })
+    }
 
-        val imageView = findViewById<ImageView>(R.id.ivImage)
-        Glide.with(this).load(R.drawable.tokoloshietime).into(imageView)
+    private fun updateChart() {
+        // Create LineDataSet and LineData inside the callback
+        val linedata = LineDataSet(data, "Entries")
+        linechart.description.text = "Hours worked"
+        val lineset = LineData(linedata, linedata1set, linedata2set)
 
-        buttonHome?.setOnClickListener(){
-            onBackPressed()
-        }
+        // Set colors for the line
+        linedata.setColors(ColorTemplate.MATERIAL_COLORS, 255)
+        linedata.valueTextColor = Color.WHITE
+
+        // Set the LineData to the chart
+        linechart.data = lineset
+
+        // Notify the chart that the data has changed
+        linechart.notifyDataSetChanged()
+        linechart.invalidate()
     }
 }
+
